172e5e744264e69093b5a0758ee30ffab5e000a3
Xumee <samusandoval795@gmail.com>
2026-02-04 19:24:00 -0500

    Use environment variable for server port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const app = require('./src/app');
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });

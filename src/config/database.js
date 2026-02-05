5b1ce69f28312c2be3293982e89419acb22819b6
Xumee0 <samusandoval795@gmail.comm>
2026-02-04 23:04:36 -0500

    cambio7

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,6 @@
 const mysql = require('mysql2/promise');
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  waitForConnections: true,
-  connectionLimit: 5,
-  queueLimit: 0
-});
+const pool = mysql.createPool(process.env.MYSQL_PUBLIC_URL);
 
 // =========================
 // PRUEBA DE CONEXIÓN (TEMPORAL)

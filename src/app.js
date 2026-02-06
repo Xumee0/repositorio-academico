b97a94000a06a5ca567806bf37d1394ecef069fd
Xumee0 <samusandoval795@gmail.comm>
2026-02-06 05:08:27 -0500

    Cambio 33

--- src/app.js
+++ src/app.js
@@ -28,7 +28,8 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
 // Uploads
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+
 
 // =========================
 // RUTA RAÍZ
